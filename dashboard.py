96ed6fe71d5c9c0a988f4b66b43cb45d800c05b2
Delorme <delormebenoit211@gmail.com>
2021-09-07 18:29:55 +0200

    Name of original train csv file

--- dashboard.py
+++ dashboard.py
@@ -1,28 +1,52 @@
+# --------------------
+# IMPORTS
+# --------------------
 import pandas as pd
 import streamlit as st
 
 
 
+# --------------------
+# CONSTANTS
+# --------------------
+#IMPORTANT_FEATURES = 
+
+
+
+@st.cache
+def load_data(path):
+    dataframe = pd.read_csv(path)
+    return dataframe
 
 
 
 
+# --------------------
+# INPUTS
+# --------------------
+# Choose the applicant
+applicant_id = st.sidebar.selectbox(
+    'Applicant identification number',
+    test_df.index)
+
+# Get the 
 add_selectbox = st.sidebar.slider('parametre_1', 0, 100, 25)
-add_selectbox = st.sidebar.slider('parametre_2', 0, 100, 25)
+
 add_selectbox = st.sidebar.selectbox(
     'parametre_3',
     ('valeur_A', 'valeur_B', 'valeur_C'))
-add_selectbox = st.sidebar.selectbox(
-    'parametre_4',
-    ('valeur_X', 'valeur_Y', 'valeur_Z'))
 
 
+
+# --------------------
+# OUTPUTS
+# --------------------
 col1, col2 = st.columns(2)
 with col1:
-    st.header("Allocation du crédit")
+    st.header("Credit status")
     with st.form("my_form_1"):
-        st.subheader("Indicateur ROV")
-        st.subheader("Décision")
+        st.subheader("rov Indicator")
+        st.subheader("Decision")
         st.form_submit_button('')
 
 with col2:
