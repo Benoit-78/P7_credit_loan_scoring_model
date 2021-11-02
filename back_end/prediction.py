cadd2299d85af62e8ddd703a7ff7cd7618242f4e
Benoit DELORME <delormebenoit211@gmail.com>
2021-11-02 16:26:14 +0100

    with tree/main

--- back_end/prediction.py
+++ back_end/prediction.py
@@ -74,9 +74,9 @@ def load_data(path, file_name):
 
 def model_path(path):
     if is_local(path):
-        model_path = path + '\\'
+        model_path = path + '\\back_end\\'
     else:
-        model_path = path + '/'
+        model_path = path + '/tree/main/back_end/'
     model_path += 'fitted_gbstg.pkl'
     return model_path
 
